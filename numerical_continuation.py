c33e28c008010281a35b09849f1ee6ad6ca79333
Mark Blyth <blyth87@gmail.com>
2020-12-15 14:50:11 +0000

    PEP8 formatting

--- numerical_continuation.py
+++ numerical_continuation.py
@@ -11,7 +11,9 @@ INITIAL_PARS = (1, 2)
 PAR_RANGE = [1, 10]
 
 
-def estimate_initial_continuation_vector(continuation_obj, parameter, period, initial_cond):
+def estimate_initial_continuation_vector(
+    continuation_obj, parameter, period, initial_cond
+):
     """
     Given a parameter value, an initial condition assumed to be
     close to the limit cycle for that parameter value, and an
@@ -24,9 +26,13 @@ def estimate_initial_continuation_vector(continuation_obj, parameter, period, in
     trajectory values at the meshpoints. These state values are
     then reshaped to get the continuation vector.
     """
+
     def ode(t, y):
         return continuation_obj.ode_RHS(y, parameter)
-    soln = scipy.integrate.solve_ivp(ode, [0, period], initial_cond, dense_output=True).sol
+
+    soln = scipy.integrate.solve_ivp(
+        ode, [0, period], initial_cond, dense_output=True
+    ).sol
     coeffs_list = soln(period * continuation_obj.col_func.full_mesh).T
     target_shape = (continuation_obj.col_func.n_subintervals, -1, coeffs_list.shape[1])
     coeffs_list.reshape(target_shape)
@@ -59,8 +65,8 @@ def get_starters(guess_vec_1, guess_vec_2, continuer):
 def hopf(state, par, stable_LC=True):
     sigma = -1 if stable_LC else 1
     x, y = state.T
-    x_dot = par * x - y + sigma * x * (x**2 + y**2)
-    y_dot = x + par * y + sigma * y * (x**2 + y**2)
+    x_dot = par * x - y + sigma * x * (x ** 2 + y ** 2)
+    y_dot = x + par * y + sigma * y * (x ** 2 + y ** 2)
     return np.vstack((x_dot, y_dot)).T
 
 
@@ -78,13 +84,19 @@ def main():
     krogh = collocation.KroghMesh(MESH_SIZE, POLYNOMIAL_ORDER)
     continuer = collocation.NumericalContinuation(hopf, krogh)
 
-    initial_vec_1 = estimate_initial_continuation_vector(continuer, INITIAL_PARS[0], 6.2, [0, np.sqrt(INITIAL_PARS[0])])
-    initial_vec_2 = estimate_initial_continuation_vector(continuer, INITIAL_PARS[1], 6.2, [0, np.sqrt(INITIAL_PARS[1])])
+    initial_vec_1 = estimate_initial_continuation_vector(
+        continuer, INITIAL_PARS[0], 6.2, [0, np.sqrt(INITIAL_PARS[0])]
+    )
+    initial_vec_2 = estimate_initial_continuation_vector(
+        continuer, INITIAL_PARS[1], 6.2, [0, np.sqrt(INITIAL_PARS[1])]
+    )
     starters = get_starters(initial_vec_1, initial_vec_2, continuer)
     print("Starters:")
     print(starters)
 
-    continuation_vectors, _ = continuer.run_continuation(starters, solver, par_range=PAR_RANGE)
+    continuation_vectors, _ = continuer.run_continuation(
+        starters, solver, par_range=PAR_RANGE
+    )
 
     fig, ax = plt.subplots()
     ts = np.linspace(0, 1)
